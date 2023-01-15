dce2cce7bcf7c3b43423110b966fb8c512ad8d4e
Ilya Choroszun <choroshun01@gmail.com>
2023-01-15 16:16:53 +0100

    Update/delete posts, other post, customize posts.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # protected-note-app
  
+Opis projektu:
+Aplikacja pozwala zalogowanemu użytkownikowi na przechowywanie notatek tekstowych. Niektóre notatki mogą być zaznaczone jako zaszyfrowane. Odszyfrowanie powinno wymagać podania tajnego hasła.
+
+Użytkownik powinien móc się zarejestrować i zalogować za pomocą hasła.
+Aplikacja pozwala na udostępnienie innym użytkownikom wybranych notatek (nieszyfrowanych). Dopuszczane są dwa rozwiązania:
+
+    -notatki udostępnione wybranym użytkownikom,
+    -notatki udostępnione publicznie.
+
+Notatki muszą pozwalać na podstawowe ostylowanie, co najmniej: dodanie pogrubienia wybranego słowa, dodanie obrazka z zewnętrznego serwisu, dodanie odnośnika, dodanie pochylenia wybranego słowa, dodanie nagłówku o wybranym poziomie (1-5).
+
+Moduł uwierzytelniania powinien zakładać:
+
+    - walidację danych wejściowych (z negatywnym nastawieniem),
+    - opóźnienia i limity prób (żeby utrudnić zdalne zgadywanie i atak brute-force),
+    - ograniczone informowanie o błędach (np. o tym przyczynie odmowy uwierzytelenia),
+    - bezpieczne przechowywanie hasła (wykorzystanie kryptograficznych funcji mieszających, wykorzystanie soli, wielokrotne hashowanie)
+    - kontrola siły hasła, żeby uświadomić użytkownikowi problem
+    - zarządzanie uprawnieniami do zasobów.
+
+Przed rozpoczęciem pracy nad projektem należy załączyć w ISODzie dokument, w którym zostanie opisany planowany stos technoloiczny, architektura, wykorzystane algorytmy i kluczowe decyzje projektowe.
+
+Na koniec należy przygotować krótką prezentację (5 min.). Kod musi zostać udostępniony do wglądu prowadzącemu przed prezentacją. Prezentacja może zawierać jako ostatni slajd bibliografię.
+
+Wymagania:
+
+    - skonteneryzowanie przy pomocą Docker
+    - $ docker-compose up lub $ sh run-docker.sh (w przypadku niezrealizowania tego podpunktu aplikacja jest oceniana za maksymalnie 15pkt)
+    - aplikacja posiada bazę danych (SQL, może być SQLite),
+    - bezpieczne połączenie z aplikacją (szyfrowane połączenie),
+    - wszystkie dane wejściowe od użytkownika podlegają walidacji z negatywnym nastawieniem,
+    - weryfikowany jest dostęp użytkowników do zasobów,
+    - weryfikacja liczby nieudanych prób logowania,
+    - sprawdzanie jakości hasła (np. jego entropii),
+    - dodać opóźnienie podczas logowania,
+    - wykorzystując szkielet aplikacji (czy moduł) należy dokładnie wiedzieć jak jest on zaimplementowany.
+
+Elementy dodatkowe (pożądane):
+
+    - zabezpieczenie przeciwko Cross-Site Request Forgery (żetony CSRF/XSRF),
+    - możliwość odzyskania dostępu w przypadku utraty hasła:
+    - Użytkownik poprosił o zmianę hasła, wysłałbym mu link: ......
+    - na adres e-mail: .....
+    - monitorowanie pracy systemu (np. żeby poinformować użytkownika o nowych komputerach, które łączyły się z jego kontem)
+    - zostawienie honeypots,
+    - mechanizm Content-Security-Policy,
+    - wyłączenie nagłówka Server.
--- main/routes.py
+++ main/routes.py
@@ -8,6 +8,7 @@ from main import app, db, bcrypt
 from main.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm, DecryptForm
 from main.cipher import encrypt, decrypt, generate_iv
 from main.entrophy import calculate_entrophy
+from main.parser import parse
 from main.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 from sqlalchemy import or_, and_
@@ -24,6 +25,11 @@ def find_viewers(text):
     matches = [t.strip('@;,!?.') for t in text.split() if t.startswith('@')]
     print(matches)
     return matches
+
+
+@app.template_filter('tag_parser')
+def sanitize(s):
+    return parse(s).unescape()
 
 
 @app.route("/")
@@ -211,3 +217,100 @@ def post(post_id):
                 flash('You do not have an access to that post', 'danger')
                 return redirect(url_for('index'))
         return render_template('post.html', title=post.title, post=post, form=form)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+
+        new_content = form.content.data
+        if post.encrypt:
+            if not (form.password.data):
+                form.encrypt.data = True
+                flash(f'Provide password', 'danger')
+                return render_template('full_create_post.html', title="Update post",
+                                       form=form, legend='Update Post', require_pass=True)
+            if (form.content.data == '***content encrypted***'):
+                try:
+                    form.content.data = decrypt(form.password.data, post.content, salt=post.title, iv=post.iv)
+                except ValueError:
+                    flash(f'Provided password is incorrect', 'danger')
+                    return redirect(url_for('post', post_id=post.id))
+                return render_template('full_create_post.html', title="Update post",
+                                       form=form, legend='Update Post', require_pass=True)
+
+            new_content = encrypt(form.password.data, new_content, salt=form.title.data, iv=post.iv)
+
+        post.title = form.title.data
+        post.content = new_content
+
+        if post.group_note:
+            potential_viewers = find_viewers(new_content)
+            for viewer in post.viewers:
+                if not viewer.username in potential_viewers:
+                    post.viewers.remove(viewer)
+
+        if not post.encrypt:
+            potential_viewers = find_viewers(new_content)
+            print(potential_viewers)
+            for v in potential_viewers:
+                viewer = User.query.filter_by(username=v).first()
+                if viewer and not post.viewers.count(viewer):
+                    post.group_note = True
+                    post.viewers.append(viewer)
+
+        db.session.commit()
+        flash('Post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+        if post.encrypt:
+            form.content.data = '***content encrypted***'
+            form.encrypt.data = True
+            return render_template('full_create_post.html', title="Update post",
+                                   form=form, legend='Update Post', require_pass=True)
+
+    return render_template('create_post.html', title="Update post",
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+
+    if current_user.is_authenticated and not current_user.is_anonymous and current_user.is_active and current_user.username:
+        posts = db.session.query(Post).filter(
+            and_(or_(and_(Post.encrypt == False, Post.group_note == True, Post.author == current_user),
+                     and_(Post.encrypt == False, Post.group_note == True, Post.viewers.any(id=current_user.id)),
+                     and_(Post.encrypt == False, Post.group_note == False),
+                     and_(Post.author == current_user, Post.encrypt == True))), Post.author == user) \
+            .order_by(Post.date_posted.desc())
+        for post in posts:
+            if post.encrypt:
+                post = show_as_encrypted(post)
+        posts = posts.paginate(page=page, per_page=5)
+    else:
+        posts = db.session.query(Post).filter(
+            and_(Post.group_note == False, Post.encrypt == False, Post.author == user)).order_by(
+            Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("user_post.html", posts=posts, user=user)
